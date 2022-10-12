ac0262c4ee9894f49d6f9d0f59511a98b087339b
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-12 11:22:44 +0800

    コメント

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,20 +1,27 @@
 //use active_webとは、active_web::prelude::*と同じ意味
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+// https://actix.rs/docs/getting-started/
+//　上記の基本
+
+// GET /hello
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+// POST /echo
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+//
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+//#[]とは、マクロを表す
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
