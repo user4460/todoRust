6ce5ec5b9c1c7057413006806010b641084671f2
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-20 09:50:21 +0800

    nixで編集

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
-//shell.nixとは、nix-shellで実行されるコマンドを記述するファイルです。
-//nixとは、NixOSのシェルを使うための設定ファイルです。
+#shell.nixとは、nix-shellで実行されるコマンドを記述するファイルです。
+#nixとは、NixOSのシェルを使うための設定ファイルです。
 { dappPkgs ? (
     import (fetchTarball "https://github.com/makerdao/makerpkgs/tarball/master") {}
   ).dappPkgsVersions.hevm-0_43_1
 }: with dappPkgs;
 
+#mkShellとは、nix-shellで実行されるコマンドを記述する関数です。
 mkShell {
   DAPP_SOLC = solc-static-versions.solc_0_6_12 + "/bin/solc-0.6.12";
   DAPP_TEST_ADDRESS = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
